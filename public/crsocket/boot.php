9d4bd48d9e0213bc18f128c50821d82dd1097b0e
henrikdvn <56581628+henrikdvn@users.noreply.github.com>
2023-08-02 15:55:30 +0200

    Updated boot-script documentation

--- public/crsocket/boot.php
+++ public/crsocket/boot.php
@@ -1,11 +1,26 @@
 <?php
 error_reporting(E_ALL);
 
-// Must point to to the location of the crsocket root directory.
+/*
+ ------------------------------------------------------------------------------------------------------------------
+ This script defines constants used by all CRSocket resources
+ ------------------------------------------------------------------------------------------------------------------
+ */
+
+// This constant must point to to the location of the private crsocket root directory.
 define("CRSOCKET_ROOT", exec("echo ~")."/neurolab_no/crsocket");
 
+// The CRSocket PHP library.
 define("CRSOCKET_PHP", CRSOCKET_ROOT."/lib/php");
+
+// The CRSocket log directory, requires write access for public PHP scripts.
 define("CRSOCKET_LOG", CRSOCKET_ROOT."/log");
+
+// The CRSocket data directory, requires write access for public PHP scripts.
 define("CRSOCKET_DATA", CRSOCKET_ROOT."/data");
+
+// The CRSocket socket directory, requires write access for public PHP scripts.
 define("CRSOCKET_SOCK", CRSOCKET_ROOT."/sock");
+
+// The current resource name ("clientid", "eventsource" or "sendevent"), derived from the parent directory of the requested PHP script.
 define("RESOURCE_NAME", basename(dirname($_SERVER["SCRIPT_FILENAME"])));
--- public/crsocket/mathtrials/boot.php
+++ public/crsocket/mathtrials/boot.php
@@ -1,7 +1,25 @@
 <?php
 error_reporting(E_ALL);
 include_once("../../boot.php");
+
+/*
+ ------------------------------------------------------------------------------------------------------------------
+ This script defines app-specific constants used by CRSocket resources
+ ------------------------------------------------------------------------------------------------------------------
+ */
+
+// The current application id (eg. "commtest" or "mathtrials"), derived from the parent directory of this script.
+
 define("APP_ID", basename(__DIR__));
+
+// The PHP library used by the current app.
+
 define("APP_PHP", CRSOCKET_ROOT."/".APP_ID."/lib/php");
+
+// The log directory used by the current app, requires write access for public PHP scripts.
+
 define("APP_LOG", CRSOCKET_ROOT."/".APP_ID."/log");
+
+// The data directory used by the current app, requires write access for public PHP scripts.
+
 define("APP_DATA", CRSOCKET_ROOT."/".APP_ID."/data");
